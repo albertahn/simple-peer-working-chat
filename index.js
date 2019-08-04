9b2364c28a8dba067ff933522abedb005a8dd205
albertpoynt <albert@poynt.co>
2019-08-04 15:27:38 -0700

    working

--- index.js
+++ index.js
@@ -1,150 +1,112 @@
 
-var getUserMedia = require('getusermedia')
+//var getUserMedia = require('getusermedia')
+var Peer = require('simple-peer')
+var video = document.createElement('video')
+    document.body.appendChild(video)
+  // getMedia();
+
+   /* function getMedia() {
+      let stream = null;
+    
+      try {
+        stream = await navigator.mediaDevices.getUserMedia(constraints);
+       
+        gotMedia(stream);
+      } catch(err) {
+       
+      }
+    } */
+    /* navigator.mediaDevices.getUserMedia({ video: true, audio: true }, gotMedia, () => {
+
+
+
+
+    }) */
+    var constraints = { audio: true, video: { width: 1280, height: 720 } }; 
+    navigator.mediaDevices.getUserMedia(constraints)
+    .then(function(stream) {
+      /* use the stream */
+      gotMedia(stream);
+    })
+    .catch(function(err) {
+      /* handle the error */
+      console.log(err);
+    });
 
 
 // get video/voice stream
-navigator.getUserMedia({ video: true, audio: true }, gotMedia, () => {})
-
-  /* var Peer = require('simple-peer')
-  var peer = new Peer({
-    initiator: location.hash === '#init',
-    trickle: false,
-    stream: stream
-  })
- */
+//todo : navigator.getUserMedia({ video: true, audio: true }, gotMedia, () => {})
+/* getUserMedia({video: true, audio: true}, function (err, stream) {
+  // if the browser doesn't support user media
+  // or the user says "no" the error gets passed
+  // as the first argument.
+  if (err) {
+     console.log('failed mediaman');
+  } else {
 
 var Peer = require('simple-peer')
-//var wrtc = require('wrtc')
-//me
-
-
-/* 
-peer1.on('signal', function (data) {
-    console.log("signal : ");
-    document.getElementById('yourId').value = JSON.stringify(data)
-    peer2.signal(data);
-    console.log("signal JSON.stringify(data): "+JSON.stringify(data));
-  })
-
-  otherpeer2.on('signal', data => {
-    // when otherpeer2 has signaling data, give it to peer1 somehow
-    peer1.signal(data)
-  })
-
-  peer1.on('connect', () => {
-    // wait for 'connect' event before using the data channel
-    console.log("peer1.on('connect': ");
-    peer1.send('hey peer2, how is it going?')
-  })
-
-
-  otherpeer2.on('data', data => {
-    // got a data channel message
-    console.log('got a message from peer1: ' + data)
-  })
-
-  document.getElementById('connect').addEventListener('click', function () {
-    var otherId = JSON.parse(document.getElementById('otherId').value)
-    
-    peer1.signal(otherId)
-  }) */
-
-  /* document.getElementById('send').addEventListener('click', function () {
-    var yourMessage = document.getElementById('yourMessage').value
-    console.log("yourMessage JSON.stringify(data): "+JSON.stringify(yourMessage));
-    peer1.send(yourMessage)
-  }) */
-
-  /* peer1.on('data', function (data) {
-    console.log("data JSON.stringify(data): "+JSON.stringify(data));
-    document.getElementById('messages').textContent += data + '\n'
-  }) */
-
-  /* peer1.on('stream', function (stream) {
-    console.log("streamstream");
-    var video = document.createElement('video')
-    document.body.appendChild(video)
-    video.src = window.URL.createObjectURL(stream)
-    video.play()
-  }) */
-
   var video = document.createElement('video')
     document.body.appendChild(video)
-  
-  function gotMedia (stream) {
+    gotMedia(stream);
+  }
 
-    var peer1 = new Peer(
-      {
-        
-        initiator: location.hash === '#host',
-        trickle: false,
-        //wrtc: wrtc,
-        stream: stream 
-      })
-    //var otherpeer2 = new Peer({ wrtc: wrtc })
-
-    //var peer1 = new Peer({ initiator: true, stream: stream })
-    //var otherpeer2 = new Peer()
-  
-    peer1.on('signal', data => {
-      //otherpeer2.signal(data)
-
-      //GET MY DATA ON THE 
-      document.getElementById('yourId').value = JSON.stringify(data)
-
-    })
-  
-    /* otherpeer2.on('signal', data => {
-      peer1.signal(data)
-    }) */
-
-    document.getElementById('connect').addEventListener('click', function () {
-      var otherId = JSON.parse(document.getElementById('otherId').value)
-      peer1.signal(otherId)
-
-      alert('connect: '+otherId);
-    })
-  
-
-    /* otherpeer2.on('data', data => {
-      // got a data channel message
-      console.log('got a message from peer1: ' + data)
-    }) */
-
-    peer1.on('data', data => {
-
-      document.getElementById('messages').textContent += data + '\n'
-
-      // got a data channel message
-      console.log('got a message from peer1: ' + data)
-    })
-  
-    peer1.on('stream', stream => {
-      // got remote video stream, now let's show it in a video tag
-      var video = document.querySelector('video')
-  
-      if ('srcObject' in video) {
-        video.srcObject = stream
-      } else {
-        video.src = window.URL.createObjectURL(stream) // for older browsers
-      }
-  
-      video.play()
-    })
+}); */
 
 
-    /* window.localConnection = localConnection =
-          new RTCPeerConnection(configuration); // configuration.iceServer
+/* // first deal with browser prefixes
+var getUserMedia = navigator.getUserMedia || 
+    navigator.mozGetUserMedia || 
+    navigator.webkitGetUserMedia;
 
-      localChannel = localConnection.createDataChannel('data');
-      localChannel.onopen = function () { // never triggered
-        var readyState = localChannel.readyState;
-        console.log('Local channel state is: ' + readyState);
-      } */
+    // make sure it's supported and bind to navigator
+if (getUserMedia) {
+     gotMedia = getUserMedia.bind(navigator);
+} else {
+  // have to figure out how to handle the error somehow
+} */
 
-      document.getElementById('send').addEventListener('click', function () {
-        var yourMessage = document.getElementById('yourMessage').value
-        console.log("yourMessage JSON.stringify(data): "+JSON.stringify(yourMessage));
-        peer1.send(yourMessage)
-      })
+
+ function gotMedia (stream) {
+              var peer1 = new Peer(
+                {
+                  initiator: location.hash === '#host',
+                  trickle: false,
+                  //wrtc: wrtc,
+                  stream: stream 
+                })
+              peer1.on('signal', data => {
+                //GET MY DATA ON THE 
+                document.getElementById('yourId').value = JSON.stringify(data)
+              })
+
+              document.getElementById('connect').addEventListener('click', function () {
+                var otherId = JSON.parse(document.getElementById('otherId').value)
+                peer1.signal(otherId)
+
+                alert('connect: '+otherId);
+              })
+              peer1.on('data', data => {
+
+                document.getElementById('messages').textContent += data + '\n'
+                console.log('got a message from peer1: ' + data)
+              })
+            
+              peer1.on('stream', stream => {
+                // got remote video stream, now let's show it in a video tag
+                var video = document.querySelector('video')
+            
+                if ('srcObject' in video) {
+                  video.srcObject = stream
+                } else {
+                  video.src = window.URL.createObjectURL(stream) // for older browsers
+                }
+            
+                video.play()
+              })
+
+                document.getElementById('send').addEventListener('click', function () {
+                  var yourMessage = document.getElementById('yourMessage').value
+                  console.log("yourMessage JSON.stringify(data): "+JSON.stringify(yourMessage));
+                  peer1.send(yourMessage)
+                })
   }
